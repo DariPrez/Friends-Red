3d936e7b0d6dee5e11dfe3b455fd8821241cb1bc
Dariana <dari_p@msn.com>
2017-09-14 20:55:14 +0200

    Commit Little Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ProfileIndexComponent } from './profile-index/profile-index.component';
+
+
+const appRoutes: Routes = [
+  { path: 'order', component: '' },
+  {
+    path: 'profile',
+    component: 'ProfileIndexComponent',
+    data: { title: 'Friends Red' }
+  },
+  { path: '',
+    redirectTo: '/profile',
+    pathMatch: 'full'
+  }
+  // { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileIndexComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
+    // other imports here
   ],
   providers: [],
   bootstrap: [AppComponent]
